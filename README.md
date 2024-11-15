67e8f7aae7ae3fc9d192185897f57a8f03b25f4a
yunsell <yunsell31@gmail.com>
2024-11-15 13:39:28 +0900

    - init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# multicampus
+# 멀티캠퍼스
+
+---
+## [Ctrl+AI] 생성형 AI 3대장을 내 업무에 적용하는 방법 - ChatGPT, Gemini, Clova X
+![img.png](img.png)
---
+++ main.py
@@ -0,0 +1,159 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from typing import Dict
+import time
+import requests
+import json
+import google.generativeai as genai
+from fastapi.middleware.cors import CORSMiddleware
+from openai import OpenAI
+from config import Config
+
+OPENAI_API_KEY = Config.read('app', 'gpt.key')
+CLOVAX_API_KEY = Config.read('app', 'clova.key')
+CLOVAX_PRIMARY_KEY = Config.read('app', 'clova.primary.key')
+GEMINI_API_KEY = Config.read('app', 'gemini.key')
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+templates = Jinja2Templates(directory="templates")
+
+# ChatGPT 엔드포인트
+@app.post("/chatgpt")
+async def chatgpt_api(request: Request):
+    data = await request.json()
+    prompt = data.get('prompt')
+    keyword = data.get('keyword')
+    YOUR_API_KEY = OPENAI_API_KEY
+
+    start = time.time()
+    client = OpenAI(api_key=YOUR_API_KEY)
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": prompt},
+            {
+                "role": "user",
+                "content": keyword
+            }
+        ]
+    )
+
+    end = time.time()
+    sec = (end - start)
+    response_text = completion.choices[0].message.content
+    return {"response": response_text, "time": round(sec, 4)}
+
+
+class CompletionExecutor:
+    def __init__(self, host, api_key, api_key_primary_val, request_id):
+        self._host = host
+        self._api_key = api_key
+        self._api_key_primary_val = api_key_primary_val
+        self._request_id = request_id
+
+    def execute(self, completion_request):
+        headers = {
+            'X-NCP-CLOVASTUDIO-API-KEY': self._api_key,
+            'X-NCP-APIGW-API-KEY': self._api_key_primary_val,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id,
+            'Content-Type': 'application/json; charset=utf-8',
+            'Accept': 'text/event-stream'
+        }
+
+        with requests.post(self._host + '/testapp/v1/chat-completions/HCX-003',
+                           headers=headers, json=completion_request, stream=True) as r:
+            process_next_line = False
+            for line in r.iter_lines():
+                if line:
+                    decoded_line = line.decode("utf-8")
+                    # 'event:result' 이벤트를 감지하면, 다음 라인을 처리하기 위해 플래그 설정
+                    if 'event:result' in decoded_line:
+                        process_next_line = True
+                        continue  # 현재 라인은 무시하고 다음 라인으로 넘어감
+
+                    # 이전 라인에서 'event:result' 이벤트가 감지되었다면, 이 라인을 처리
+                    if process_next_line:
+                        result = decoded_line
+                        if result.startswith('data:'):
+                            result = result[5:]
+                        print('************************')
+                        print(result)
+                        data = json.loads(result)
+                        text = data["message"]["content"]
+                        counttokens = int(data["inputLength"]) + int(data["outputLength"])
+                        print("#" * 50)
+                        print("ClovaX: ", text)
+                        return text, counttokens
+# ClovaX 엔드포인트
+@app.post("/clovax")
+async def clovax_api(request: Request):
+    data = await request.json()
+    prompt = data.get('prompt')
+    keyword = data.get('keyword')
+
+    start = time.time()
+
+    completion_executor = CompletionExecutor(
+        host='https://clovastudio.stream.ntruss.com',
+        api_key=CLOVAX_API_KEY,
+        api_key_primary_val=CLOVAX_PRIMARY_KEY,
+        request_id='ea4e60f460b94843b1c02563bc3277f1'
+    )
+
+    preset_text = [{"role":"system","content":prompt},
+                   {"role":"user","content":keyword}]
+
+    request_data = {
+        'messages': preset_text,
+        'topP': 0.9,
+        'topK': 0,
+        'maxTokens': 256,
+        'temperature': 0.5,
+        'repeatPenalty': 5.0,
+        'stopBefore': ['###', '###'],
+        'includeAiFilters': True,
+        'seed': 0
+    }
+
+    end = time.time()
+    sec = (end - start)
+    contents, tokken = completion_executor.execute(request_data)
+    return {"response": contents, "tokens": tokken, "time": round(sec, 4)}
+
+# Gemini 엔드포인트
+@app.post("/gemini")
+async def gemini_api(request: Request):
+    data = await request.json()
+    prompt = data.get('prompt')
+    keyword = data.get('keyword')
+    YOUR_API_KEY = GEMINI_API_KEY
+
+    start = time.time()
+
+    genai.configure(api_key=YOUR_API_KEY)
+
+    model = genai.GenerativeModel('gemini-1.5-flash', system_instruction=prompt)
+
+    user_prompt = keyword
+    response = model.generate_content(
+        user_prompt,
+        generation_config=genai.types.GenerationConfig(
+            candidate_count=1,
+            temperature=1.0)
+    )
+    end = time.time()
+    sec = (end - start)
+    return {"response": response.text, "time": round(sec, 4)}
+
+# 테스트 페이지 엔드포인트
+@app.get("/test")
+async def test(request: Request):
+    return templates.TemplateResponse("test.html", {"request": request})
